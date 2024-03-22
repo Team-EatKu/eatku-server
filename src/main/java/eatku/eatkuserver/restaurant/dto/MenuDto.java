b8a1bf17b718ca032d2757fd8a0a9e1149cb529a
seongmin0229 <leesung2925@gmail.com>
2024-03-22 22:28:51 +0900

    fix : RestaurantRegisterRequestDto swagger ui example 추가

--- src/main/java/eatku/eatkuserver/restaurant/dto/MenuDto.java
+++ src/main/java/eatku/eatkuserver/restaurant/dto/MenuDto.java
@@ -1,11 +1,15 @@
 package eatku.eatkuserver.restaurant.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@Schema
 public class MenuDto {
+    @Schema(description = "menu name", example = "어쩌규 규동")
     private String name;
+    @Schema(description = "price of this menu", example = "6000")
     private Long price;
 }
--- src/main/java/eatku/eatkuserver/restaurant/dto/RestaurantRegisterRequestDto.java
+++ src/main/java/eatku/eatkuserver/restaurant/dto/RestaurantRegisterRequestDto.java
@@ -3,6 +3,8 @@ package eatku.eatkuserver.restaurant.dto;
 import eatku.eatkuserver.restaurant.domain.Category;
 import eatku.eatkuserver.restaurant.domain.Hashtag;
 import eatku.eatkuserver.restaurant.domain.Menu;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,17 +13,30 @@ import java.util.List;
 
 @Data
 @Builder
+@Schema
 public class RestaurantRegisterRequestDto {
+    @Schema(description = "restaurant name", example = "코코도리")
     private String name;
+    @Schema(description = "restaurant address", example = "서울특별시 광진구 화양동 123-123")
     private String address;
+    @Schema(description = "restaurant location", example = "후문")
     private String location;
+    @Schema(description = "restaurant phone number", example = "010-1234-1234")
     private String phoneNumber;
+    @Schema(description = "restaurant latitude", example = "123.123")
     private double latitude;
+    @Schema(description = "restaurant longitude", example = "123.123")
     private double longitude;
+    @Schema(description = "restaurant information", example = "주차가능 어쩌구 저쩌구,,\n충전이 어쩌구 저쩌구..")
     private String information;
+    @Schema(description = "restaurant daily start time", example = "17:00")
     private String startTime;
+    @Schema(description = "restaurant daily end time", example = "24:00")
     private String endTime;
+    @ArraySchema(arraySchema = @Schema(description = "menu list"), schema = @Schema(implementation = MenuDto.class))
     private List<MenuDto> menuList;
+    @ArraySchema(arraySchema = @Schema(description = "category list"), schema = @Schema(implementation = String.class))
     private List<String> categoryList;
+    @ArraySchema(arraySchema = @Schema(description = "hashtag list"), schema = @Schema(implementation = String.class))
     private List<String> hashtagList;
 }
